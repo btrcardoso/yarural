58a02a018989d70492d4252485601d15a9cfaaa4
SullyvanMoura <sullyvanmoura@hotmail.com>
2022-12-14 23:14:47 -0300

    feat: criacao de usuario pelo service

--- app/Services/UserService.ts
+++ app/Services/UserService.ts
@@ -1,6 +1,20 @@
+import User from "App/Models/User"
 
 export default class UserService {
 
+    public static async createUser(email: string, password: string) {
+        let user = await User.findBy('email', email)
+
+        if (user) {
+            return user
+        }
     
+        user = new User()
+        user.email = email
+        user.password = password
+    
+        await user.save()
+        return user
+      }
 
 }
