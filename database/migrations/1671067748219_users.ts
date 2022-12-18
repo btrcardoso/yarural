8b415ae5b824470bff1f848cbdc94cfe13a2b1f7
SullyvanMoura <sullyvanmoura@hotmail.com>
2022-12-18 13:47:50 -0300

    feat: adicao de descricao ao model do usuario

--- database/migrations/1671067748219_users.ts
+++ database/migrations/1671067748219_users.ts
@@ -10,8 +10,9 @@ export default class extends BaseSchema {
       table.string('password', 180).notNullable()
       table.string('remember_me_token').nullable()
 
-      table.string('name').notNullable()
-      table.string('username').notNullable().unique()
+      table.string('name', 100).notNullable()
+      table.string('username',15).notNullable().unique()
+      table.string('description',500).nullable()
 
       /**
        * Uses timestampz for PostgreSQL and DATETIME2 for MSSQL
