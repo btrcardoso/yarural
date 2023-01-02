98179a92a2e6815ffbc10cf0a5a421ca9533d967
SullyvanMoura <sullyvanmoura@hotmail.com>
2023-01-02 11:39:11 -0300

    feat: criacao do Service de Perguntas

---
+++ app/Services/QuestionService.ts
@@ -0,0 +1,22 @@
+import Question from "App/Models/Question"
+
+export default class QuestionService {
+
+    public static async createQuestion(question: string, description: string) {
+    
+        const quest = new Question()
+        quest.question = question
+        quest.description = description
+
+        await quest.save()
+        return quest
+    }
+
+    public static async destroyQuestion(id: number) {
+        
+        const quest = await Question.findOrFail(id)
+        quest.delete()
+
+    }
+
+}
