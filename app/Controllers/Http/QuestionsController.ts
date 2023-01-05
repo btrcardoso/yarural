aa88b8098488b644c1015292fa956a07ae9d179e
SullyvanMoura <sullyvanmoura@hotmail.com>
2023-01-05 14:02:05 -0300

    feat: envio do usuario para a tela da pergunta

--- app/Controllers/Http/QuestionsController.ts
+++ app/Controllers/Http/QuestionsController.ts
@@ -1,5 +1,6 @@
 import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
 import Question from 'App/Models/Question'
+import User from 'App/Models/User'
 import QuestionService from 'App/Services/QuestionService'
 import QuestionValidator from 'App/Validators/QuestionValidator'
 
@@ -23,7 +24,8 @@ export default class QuestionsController {
 
   public async show({ params, view }: HttpContextContract) {
     const quest = await Question.findOrFail(params.id)
+    const user = await User.findOrFail(quest.userId)
 
-    return view.render('questions/show', { question: quest })
+    return view.render('questions/show', { question: quest , user: user})
   }
 }
