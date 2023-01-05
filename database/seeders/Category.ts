303734dc9e2cae2173aa7cd8811f21d98f44d191
Beatriz <btrcardos@gmail.com>
2023-01-05 18:08:28 -0300

    feat: criacao do model, validator, migration e seeder de categoria

---
+++ database/seeders/Category.ts
@@ -0,0 +1,18 @@
+import BaseSeeder from '@ioc:Adonis/Lucid/Seeder'
+import Category from 'App/Models/Category'
+
+export default class extends BaseSeeder {
+  public async run () {
+    await Category.createMany([
+      {
+        name: 'Secretaria'
+      },
+      {
+        name: 'Bandeijão'
+      }, 
+      {
+        name: 'Matrícula'
+      }
+    ])
+  }
+}
