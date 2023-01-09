7d93ec1d453c7e20c0f18b2375e02d04d331abe3
SullyvanMoura <sullyvanmoura@hotmail.com>
2023-01-09 17:37:34 -0300

    feat: listar perguntas na tela inicial

--- app/Controllers/Http/HomeController.ts
+++ app/Controllers/Http/HomeController.ts
@@ -1,12 +1,17 @@
 import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
+import Question from 'App/Models/Question'
 
 export default class HomeController {
 
     public async index({ auth, view }: HttpContextContract){
         if (auth.use('web').isLoggedIn){
-            return view.render('home/loggedInHome')
+
+            const questions = await Question.query().orderBy('created_at', 'desc').paginate(1,10)
+
+            return view.render('home/loggedInHome', {questions: questions})
         }
         else{
+
             return view.render('home/notLoggedInHome')
         }
     }
