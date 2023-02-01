ecf9e57e2d4628ba605e22626d5617c239713ae0
Beatriz <btrcardos@gmail.com>
2023-02-01 18:52:42 -0300

    refactor: utilizacao da funcao QuestionService.getQuestionWithLikes em CategoriesController

--- app/Controllers/Http/CategoriesController.ts
+++ app/Controllers/Http/CategoriesController.ts
@@ -1,6 +1,7 @@
 import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
 import Category from 'App/Models/Category'
 import Question from 'App/Models/Question'
+import QuestionService from 'App/Services/QuestionService'
 
 export default class CategoriesController {
 
@@ -11,11 +12,15 @@ export default class CategoriesController {
         return view.render('categories/index', {categories: categories})
     }
 
-    public async show({ view, params, request }: HttpContextContract){
+    public async show({ view, params, request, auth }: HttpContextContract){
         const page = request.input('page', 1)
         const limit = 10
 
-        const questions = await Question.query().where('categoryId', params.id).preload('user').orderBy('createdAt', 'desc').paginate(page, limit)
+        let questions = await Question.query().where('categoryId', params.id).preload('user').orderBy('createdAt', 'desc').paginate(page, limit)
+        for(let question of questions){
+            question = await QuestionService.getQuestionWithLikes(question, auth.user!.id)
+        }
+        
         const category = await Category.findOrFail(params.id)
 
         return view.render('categories/show', {questions: questions, category: category})
